b09b4bda48a5ea5499391505e9acbbfe0464ccb6
EfanA-12 <efanadli0@gmail.com>
2025-10-05 07:05:59 +0700

    Upload Tugas weak 1

---
+++ praktikum/week1-setup-hello-pos/src/HelloFunctional.java
@@ -0,0 +1,8 @@
+public class HelloFunctional {
+   public static void main(String[] args) {
+      String nim = "240202860";
+      String nama = "Efan Aryanto Adli";
+      System.out.println("Hello World");
+      System.out.println("I am: " + nama + ", NIM: " + nim);
+   }
+}
